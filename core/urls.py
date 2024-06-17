3857448c71c78c0fd4596ec626a79a14b928c58a
Jill Johnson <jill.johnson1337@gmail.com>
2024-06-17 10:47:23 -0500

    first commit - student plotting template

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    #homepage
+    path('', views.chart, name='chart')
     
+    #template for a new page to redirect to
+    #path('plot1/', views.plot1, name='plot1')
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+#import plotting libraries
+import pandas as pd
+import plotly.express as px
+
+#Read in CSV as a DataFrame
+df = pd.read_csv('core/data/co2_mm_mlo.csv')
+
+#function for data analysis of CSV - choose one graph / feature per function.
+def chart(request):
+
+    #plot variables into a graph
+    fig = px.line(
+        
+        df, #the dataframe you read in
+        x="year", #set x variable to date column
+        y="average", #set y variable to average column
+        title='Average C02 Levels by Day', #create title for plot
+        markers=True, #add point markers to plot
+
+    )
+
+    #convert graph to an html object
+    chart = fig.to_html()
+
+    #send html object to the url page
+    context = {'chart': chart}
+    return render(request, 'core/chart.html', context)
